c40d693975c6039be25ef253d8ce5ea9a0a4167e
Kumar Chidanandam <98702968+Kumarchidand@users.noreply.github.com>
2024-03-16 23:52:45 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (10.74%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,21 +1,19 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+       
         int n=digits.length;
-        if(digits[n-1]!=9){
-            digits[n-1]+=1;
-            return digits;
+        for(int i=n-1;i>=0;i--){
+             // if num is less than 9,inc. the  digit value 
+             if(digits[i]<9){
+               digits[i]++;
+               return digits;
+             }
+            //  if not
+             digits[i]=0;
         }
-        // if not
-        digits[n-1]=0;
-        for(int i=n-2;i>=0;i--){
-            if(digits[i]!=9){
-                digits[i]=digits[i]+1;
-                return digits;
-            }
-            digits[i]=0;
-        }
-        int [] ans=new int[n+1];
-        ans[0]=1;
-        return ans;
+        // if there is one element i.e 9 then inc its arrlength and assign the oth index to to one
+        int[] numarr=new int [n+1];
+           numarr[0]=1;
+           return numarr;
     }
 }
