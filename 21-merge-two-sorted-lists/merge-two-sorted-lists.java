bbaf3e4d7ef9c888eb6ce71a26e68b53fd631417
Kumar Chidanandam <98702968+Kumarchidand@users.noreply.github.com>
2024-05-29 01:22:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (28.24%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,31 +10,52 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode temp1=list1;
-        ListNode temp2=list2;
-        ListNode head=new ListNode(100);
-        ListNode temp=head;
-        while(temp1!=null && temp2!=null){
-            if(temp1.val<temp2.val){
-                // add in list
-                ListNode a=new ListNode(temp1.val);
-                temp.next=a;
-                temp=a;
-                temp1= temp1.next;
-            }
-            else{  
-                // add in list
-                ListNode a=new ListNode(temp2.val);
-                temp.next=a;
-                temp=a;
-                temp2= temp2.next;
-            }
+        // ListNode temp1=list1;
+        // ListNode temp2=list2;
+        // ListNode head=new ListNode(100);
+        // ListNode temp=head;
+        // while(temp1!=null && temp2!=null){
+        //     if(temp1.val<temp2.val){
+        //         // add in list
+        //         ListNode a=new ListNode(temp1.val);
+        //         temp.next=a;
+        //         temp=a;
+        //         temp1= temp1.next;
+        //     }
+        //     else{  
+        //         // add in list
+        //         ListNode a=new ListNode(temp2.val);
+        //         temp.next=a;
+        //         temp=a;
+        //         temp2= temp2.next;
+        //     }
+        // }
+        // if(temp2==null){
+        //     temp.next=temp1;
+        // }else{
+        //     temp.next=temp2;
+        // }
+        // return head.next;
+        ListNode t1=list1;
+        ListNode t2=list2;
+        ListNode t=new ListNode(100);
+        ListNode h=t;
+        while(t1!=null && t2!=null){
+           if(t1.val < t2.val){
+            t.next=t1;
+            t=t1;
+            t1=t1.next;
+           }else{
+            t.next=t2;
+            t=t2;
+            t2=t2.next;
+           }
         }
-        if(temp2==null){
-            temp.next=temp1;
+        if(t1==null){
+            t.next=t2;
         }else{
-            temp.next=temp2;
+            t.next=t1;
         }
-        return head.next;
+        return h.next;
     }
 }
