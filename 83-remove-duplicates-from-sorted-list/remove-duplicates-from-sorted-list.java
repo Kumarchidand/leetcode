ff258a33c7602a17d70fbd9df4714f14bb9581c2
Kumar Chidanandam <98702968+Kumarchidand@users.noreply.github.com>
2024-05-29 22:23:20 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (34.83%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -8,24 +8,49 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+//  using extra space O(N):sc
+// class Solution {
+//     public ListNode deleteDuplicates(ListNode head) {
+//          if(head==null || head.next==null) return head;
+//         ListNode ptr=head.next;
+//         ListNode dummy=new ListNode(head.val);
+//         ListNode ans=dummy;
+//         // dummy stores head.val eg 1
+      
+//         while(ptr!=null){
+//          if(ans.val!=ptr.val){
+//           ListNode a=new ListNode(ptr.val);
+//            ans.next=a;
+//            ans=a;
+//          ptr=ptr.next;
+//         }
+//         else{
+//             ptr=ptr.next;
+//         }
+//      } 
+//         return dummy;
+//     }
+// }
+
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
          if(head==null || head.next==null) return head;
         ListNode ptr=head.next;
         ListNode dummy=new ListNode(head.val);
         ListNode ans=dummy;
+        // dummy stores head.val eg 1
       
         while(ptr!=null){
          if(ans.val!=ptr.val){
-          ListNode a=new ListNode(ptr.val);
-           ans.next=a;
-           ans=a;
-         ptr=ptr.next;
+        //   ListNode a=new ListNode(ptr.val);
+           ans.next=ptr;
+           ans=ptr;
+           ptr=ptr.next;
         }
         else{
             ptr=ptr.next;
         }
-    } 
+     }   ans.next=null;
         return dummy;
     }
 }
